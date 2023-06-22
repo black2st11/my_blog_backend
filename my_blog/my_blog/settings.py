d49db36137df21eea2d566fedfad0798364926af
black2st11 <black2st11@gmail.com>
2023-06-22 23:08:16 +0900

    update test(hunter)

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -108,6 +108,12 @@ DATABASES = {
     },
 }
 
+if settings.DEV_ENV == "local":
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / f"{settings.PROJECT_NAME}_db.sqlite3",
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
