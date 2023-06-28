d7f4a722e0e25dec93c2d942f581071cc0ee3eb2
black2st11 <black2st11@gmail.com>
2023-06-28 22:33:16 +0900

    Fix, HttpRequest 에 _mutable attribute 가 없는 경우 발생하는 에러 수정

--- my_blog/common/viewsets.py
+++ my_blog/common/viewsets.py
@@ -9,16 +9,16 @@ class GetViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
 class IpMixin(GenericViewSet):
     def initialize_request(self, request, *args, **kwargs):
         http_request = super().initialize_request(request, *args, **kwargs)
-        if request.method == "GET":  # method get 인 경우 _mutable attr 이 없음
-            return http_request
-
-        _mutable = http_request.data._mutable
-        http_request.data._mutable = True
         x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
         if x_forwarded_for:
             ip = x_forwarded_for.split(",")[0]
         else:
             ip = request.META.get("REMOTE_ADDR")
-        http_request.data.update({"ip": ip})
-        http_request.data._mutable = _mutable
+        try:
+            _mutable = http_request.data._mutable
+            http_request.data._mutable = True
+            http_request.data.update({"ip": ip})
+            http_request.data._mutable = _mutable
+        except AttributeError:
+            http_request.data.update({"ip": ip})
         return http_request
